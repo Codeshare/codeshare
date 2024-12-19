7fcdde09cc087c8e945cacf2929a53e6f53b0a79
Tejesh Mehta <tejesh.mehta@gmail.com>
2024-12-19 11:28:58 -0800

    db .env

--- lib/clients/redis/redis.ts
+++ lib/clients/redis/redis.ts
@@ -1,17 +1,17 @@
 import { Redis as UpstashRedis } from "@upstash/redis"
 import { get } from "env-var"
 
-const KV_REST_API_URL = get("KV_REST_API_URL").required().asString()
-const KV_REST_API_TOKEN = get("KV_REST_API_TOKEN").required().asString()
+const REDIS_KV_REST_API_URL = get("KV_REST_API_URL").required().asString()
+const REDIS_KV_REST_API_TOKEN = get("KV_REST_API_TOKEN").required().asString()
 
 export type Redis = UpstashRedis
 
 export const rateLimitRedisClient = new UpstashRedis({
-  url: KV_REST_API_URL,
-  token: KV_REST_API_TOKEN,
+  url: REDIS_KV_REST_API_URL,
+  token: REDIS_KV_REST_API_TOKEN,
 })
 
 export const redisClient = new UpstashRedis({
-  url: KV_REST_API_URL,
-  token: KV_REST_API_TOKEN,
+  url: REDIS_KV_REST_API_URL,
+  token: REDIS_KV_REST_API_TOKEN,
 })
